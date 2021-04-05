d606444fba7835643d6fa85096f7c0d2a0bc0c39
Gatien Tchamda <gatien.tchamda@student.reutlingen-university.de>
2021-04-05 20:51:35 +0200

    Project Start

---
+++ src/main/java/net/javaguides/springbootbackend/model/User.java
@@ -0,0 +1,8 @@
+package net.javaguides.springbootbackend.model;
+
+public class User{
+    private long id; 
+    private String firstname; 
+    private String lastname;
+    private String email;
+}
