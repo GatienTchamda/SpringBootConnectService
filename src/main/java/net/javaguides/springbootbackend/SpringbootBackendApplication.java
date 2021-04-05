9cf687a457b1f9f34b10cd6d475304795bcd3e7b
Gatien Tchamda <gatien.tchamda@student.reutlingen-university.de>
2021-04-05 21:39:12 +0200

    Java Appliaction

--- src/main/java/net/javaguides/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springbootbackend/SpringbootBackendApplication.java
@@ -1,13 +1,29 @@
 package net.javaguides.springbootbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.javaguides.springbootbackend.model.User;
+import net.javaguides.springbootbackend.repository.UserRepository;
+
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Gatien", "Tchamda", "gat12008@yahoo.fr"));
+		this.userRepository.save(new User("Dominick", "Ndjandjo", "minkado1@yahoo.fr"));
+		this.userRepository.save(new User("Tony", "STark", "tony.stark@yahoo.fr"));
 	}
 
 }
--- src/main/java/net/javaguides/springbootbackend/controller/UserController.java
+++ src/main/java/net/javaguides/springbootbackend/controller/UserController.java
@@ -1,5 +1,22 @@
 package net.javaguides.springbootbackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springbootbackend.model.User;
+import net.javaguides.springbootbackend.repository.UserRepository;
+
+@RestController
+@RequestMapping ("api/")
 public class UserController {
-    
+
+    @Autowired
+    private UserRepository userRepository; 
+
+    @GetMapping("users")
+    public List<User> getUsers(){
+        return this.userRepository.findAll();
+    } 
 }
